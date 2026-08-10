690fd629734e0cae39a5b599f1ee01db4d841eb1
Shashank Mishra <shishankmishra22@gmail.com>
2026-08-10 19:08:52 +0530

    Add authorization token to API requests

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,8 +1,23 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://hirepro-6yde.onrender.com/api/v1",
-  withCredentials: true,
+    baseURL: "https://hirepro-6yde.onrender.com/api/v1",
+    withCredentials: true,
 });
+
+api.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem("accessToken");
+
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
 
 export default api;
