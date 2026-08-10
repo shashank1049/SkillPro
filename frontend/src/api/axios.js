f4d37360607d321759614e9d622c27b2fee561e6
Shashank Mishra <shishankmishra22@gmail.com>
2026-08-10 12:33:54 +0530

    Update production API URL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api/v1",
+  baseURL: "https://hirepro-6yde.onrender.com/api/v1",
   withCredentials: true,
 });
 
